9bf7263bd9e5d0d12f9de886f83d91498747b46c
Steve Chignell <stephen.chignell@alumni.ubc.ca>
2018-11-27 15:48:39 -0800

    Updated code

--- hist_vowels.R
+++ hist_vowels.R
@@ -1,5 +1,10 @@
+# Read in the words data
 words <- readLines("words.txt")
+
+# Count vowel frequency
 Vowels <- stringr::str_count(words, "[AaeEiIoOuU]")
+
+# Send to table
 hist_vowels <- table(Vowels)
 write.table(hist_vowels, "hist_vowels.tsv",
             sep = "\t", row.names = FALSE, quote = FALSE)
